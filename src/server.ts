b0e9bc85d416bd92635db4c231bf75c652824524
teones <teones.alex.talff99@gmail.com>
2023-05-02 16:13:50 -0300

    feat: Configure tsconfig.json

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+// import app, { init } from '@/app';
+
+// const port = +process.env.PORT || 4000;
+
+// init().then(() => {
+//   app.listen(port, () => {
+//     /* eslint-disable-next-line no-console */
+//     console.log(`Server is listening on port ${port}.`);
+//   });
+// });
+
+
+
+import app from '@/app';
+
+const port = +process.env.PORT || 4000;
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}.`);
+});
